7aeb32c418368b053a75191a018df329bca8adad
Piklo <16057987+Piklo@users.noreply.github.com>
2022-12-01 16:26:59 +0100

    Add project files.

---
+++ 2022.01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _2022._01;
+
+internal class Program
+{
+    static void Main()
+    {
+        var res1 = Solution.Solve1(Data.Value);
+        Console.WriteLine(res1);
+
+        var res2 = Solution.Solve2(Data.Value);
+        Console.WriteLine(res2);
+    }
+}
---
+++ 2022.01/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace _2022._01;
+
+public static class Solution
+{
+    private static List<int> ParseData(string data)
+    {
+        var lines = data.Split("\r\n");
+        var elvesData = new List<int>();
+
+        var currentElf = 0;
+        foreach (var line in lines)
+        {
+            if (line == "")
+            {
+                elvesData.Add(currentElf);
+                currentElf = 0;
+                continue;
+            }
+
+            var succeeded = int.TryParse(line, out var value);
+            if (!succeeded)
+            {
+                throw new Exception($"failed to get int out of \"{line}\"");
+            }
+
+            currentElf += value;
+        }
+
+        elvesData.Add(currentElf);
+
+        return elvesData;
+    }
+
+    public static int Solve1(string data)
+    {
+        var list = ParseData(data);
+
+        return list.Max();
+    }
+
+    public static int Solve2(string data)
+    {
+        var list = ParseData(data);
+        var sum = list.OrderByDescending(x => x).Take(3).Sum();
+
+        return sum;
+    }
+}
