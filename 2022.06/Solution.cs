0d35a13cb41e7be73c0caf75e1d1cbf93ab34168
Piklo <16057987+Piklo@users.noreply.github.com>
2022-12-06 14:57:26 +0100

    added solution to day 6

---
+++ 2022.06/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace _2022._06;
+internal static class Solution
+{
+    private static int Solve(string data, int marker)
+    {
+        var queue = new Queue<char>();
+        var counter = 0;
+
+        foreach (var character in data)
+        {
+            while (queue.Contains(character))
+            {
+                queue.Dequeue();
+            }
+
+            counter++;
+            queue.Enqueue(character);
+
+            if (queue.Count == marker)
+                break;
+        }
+
+        return counter;
+    }
+
+    public static int Solve1(string data)
+    {
+        const int StartOfPacketMarker = 4;
+
+        return Solve(data, StartOfPacketMarker);
+    }
+
+    public static int Solve2(string data)
+    {
+        const int StartOfMessageMarker = 14;
+
+        return Solve(data, StartOfMessageMarker);
+    }
+}
